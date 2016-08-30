cd6cee118ab44609ca976d3ccf8056afb66bd1df
Sam Baskinger <basking2@yahoo.com>
2016-08-30 15:36:57 -0500

    Making a tree project.

--- sdsai-tree/src/main/java/org/sdsai/ArrayTree.java
+++ sdsai-tree/src/main/java/org/sdsai/ArrayTree.java
@@ -16,11 +16,11 @@ import java.util.Iterator;
 public class ArrayTree<T> implements Iterable<T> {
 
     private final ArrayList<T> tree;
-    private final int branchiness;
+    private final int degree;
 
-    public ArrayTree(final int capacity, final int branchiness) {
+    public ArrayTree(final int capacity, final int degree) {
         this.tree = new ArrayList<T>(capacity);
-        this.branchiness = branchiness;
+        this.degree = degree;
     }	
 
     public T get(final int index) {
@@ -32,15 +32,15 @@ public class ArrayTree<T> implements Iterable<T> {
     }
 
     public int parent(final int index) {
-        return parent(index, branchiness);
+        return parent(index, degree);
     }
 
     public int child(final int index, final int childNum) {
-        return child(index, childNum, branchiness);
+        return child(index, childNum, degree);
     }
 
     public int child0(final int index) {
-        return child0(index, branchiness);
+        return child0(index, degree);
     }
 
     /**
@@ -51,47 +51,47 @@ public class ArrayTree<T> implements Iterable<T> {
         return tree.iterator();
     }
 
-    public static int parent(final int index, final int branchiness) {
-        return (index-1)/branchiness;
+    public static int parent(final int index, final int degree) {
+        return (index-1)/degree;
     }
 
-    public static int child0(final int index, final int branchiness) {
-        return index * branchiness + 1;
+    public static int child0(final int index, final int degree) {
+        return index * degree + 1;
     }
 
-    public static int child(final int index, final int childNum, final int branchiness) {
-        final int child0 = child0(index, branchiness);
+    public static int child(final int index, final int childNum, final int degree) {
+        final int child0 = child0(index, degree);
 
         return child0 + childNum;
     }
 
     public int levelOffset(final int level) {
-        return levelOffset(level, branchiness);
+        return levelOffset(level, degree);
     }
 
     public int indexLevel(final int index) {
-        return indexLevel(index, branchiness);
+        return indexLevel(index, degree);
     }
 
     /**
      * Starting at level 0, this returns the number of nodes at in a level.
      */
-    public static int nodesAtLevel(final int level, final int branchiness) {
-        return (int)Math.pow(branchiness, level);
+    public static int nodesAtLevel(final int level, final int degree) {
+        return (int)Math.pow(degree, level);
     }
 
     /**
      * What is the offset into an array that the first node of a tree level occures at.
      */
-    public static int levelOffset(final int level, final int branchiness) {
-        final double d = (Math.pow(branchiness, level)-1) / (branchiness-1);
+    public static int levelOffset(final int level, final int degree) {
+        final double d = (Math.pow(degree, level)-1) / (degree-1);
 
         return (int) d;
     }
 
-    public static int indexLevel(final int index, final int branchiness) {
-        final double n = Math.log(index * (branchiness - 1) + 1);
-        final double d = Math.log(branchiness);
+    public static int indexLevel(final int index, final int degree) {
+        final double n = Math.log(index * (degree - 1) + 1);
+        final double d = Math.log(degree);
         return (int)(n / d);
     }
 }
